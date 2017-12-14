28c288c57798b56d6d6c74e14c1f7857150b9509
santhosh damodharan <santhoshdamodharan@santhoshs-macbook-air.local>
2017-12-13 21:29:20 -0500

    Initial Commit

---
+++ MAPD2017-F2017-Lesson1B/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD2017-F2017-Lesson1B
+//
+//  Created by santhosh damodharan on 2017-12-13.
+//  Copyright © 2017 santhosh damodharan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
