ad775ce027a80e37c14f0094858106dbb51421d6
santhosh damodharan <santhoshdamodharan@santhoshs-macbook-air.local>
2017-12-13 22:05:42 -0500

    added HeaderCell

---
+++ MAPD2017-F2017-Lesson1B/HeaderCellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderCellCollectionViewCell.swift
+//  MAPD2017-F2017-Lesson1B
+//
+//  Created by santhosh damodharan on 2017-12-13.
+//  Copyright © 2017 santhosh damodharan. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCellCollectionViewCell: ContentCell {
+    
+}
